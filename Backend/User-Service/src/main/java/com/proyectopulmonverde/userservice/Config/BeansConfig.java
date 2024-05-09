f85822338936ad385182d3fc62e475f245619a6c
CaHuertas <cahuertascaro@gmail.com>
2024-05-08 19:26:30 -0500

    Creacion de sistema roles, con funcionalidades de admin

--- Backend/User-Service/src/main/java/com/proyectopulmonverde/userservice/Config/BeansConfig.java
+++ Backend/User-Service/src/main/java/com/proyectopulmonverde/userservice/Config/BeansConfig.java
@@ -1,11 +1,16 @@
 package com.proyectopulmonverde.userservice.Config;
 
+import com.proyectopulmonverde.userservice.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,11 +18,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class BeansConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+    }
+
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
--- Backend/User-Service/src/main/java/com/proyectopulmonverde/userservice/UserServiceApplication.java
+++ Backend/User-Service/src/main/java/com/proyectopulmonverde/userservice/UserServiceApplication.java
@@ -2,12 +2,16 @@ package com.proyectopulmonverde.userservice;
 
 import com.proyectopulmonverde.userservice.Entities.Role;
 import com.proyectopulmonverde.userservice.Repository.RoleRepository;
+import com.proyectopulmonverde.userservice.Security.RegistrationRequest;
+import com.proyectopulmonverde.userservice.Service.AuthenticationService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import static com.proyectopulmonverde.userservice.Entities.Role.*;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -18,14 +22,25 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+
     @Bean
-    public CommandLineRunner runner(RoleRepository roleRepository){
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
         return args -> {
-            if (roleRepository.findByName("USER").isEmpty()){
-                roleRepository.save(
-                        Role.builder().name("USER").build()
-                );
-            }
+            var admin = RegistrationRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@gmail.com")
+                    .password("password123")
+                    .role(ADMIN)
+                    .build();
+
+            var authResponse = service.register(admin);
+            System.out.println("Token Administrador: " + authResponse.getAccessToken());
+
         };
+
     }
+
 }
